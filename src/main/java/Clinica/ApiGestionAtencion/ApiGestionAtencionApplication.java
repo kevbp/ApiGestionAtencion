ed17296c3f00c391396595968e1ac5b5d3ce4092
Kevin Broncano <kevinbroncanop@gmail.com>
2025-12-11 00:44:50 -0500

    Update

--- src/main/java/Clinica/ApiGestionAtencion/ApiGestionAtencionApplication.java
+++ src/main/java/Clinica/ApiGestionAtencion/ApiGestionAtencionApplication.java
@@ -2,12 +2,20 @@ package Clinica.ApiGestionAtencion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiGestionAtencionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGestionAtencionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionAtencionApplication.class, args);
+    }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
